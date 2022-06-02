7d8e766adb5f1e92c9a84a0fd82bcb2c217381ba
Douglas Oliveira <douglasoliveirab11@gmail.com>
2022-06-02 12:14:18 -0300

    first commit

---
+++ 2-trabalhandoComVariáveis.js
@@ -0,0 +1,5 @@
+// console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+// JS é um case sensitivo
+const idade = 29;
+const nome = "Ricardo";
---
+++ 5-AtribuicaoDeVariaveis.js
@@ -0,0 +1,21 @@
+console.log("Trabalhando com atribuições de variáveis");
+const primeiroNome = "Douglas";
+const  sobrenome = "Oliveira";
+
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+const nomeCompleto = primeiroNome + sobrenome ; 
+console.log(primeiroNome);
+
+let contador = 0;
+contador ++;
+let idade = 30;
+idade++;
+console.log(idade);
+
+
+// console.lo(nome + "" + sobrenome);
+// não fazer 
+// mês = "Janeiro";*****//
+// let é uma variavel que pode ter o seu valor alterado
+
